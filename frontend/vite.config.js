d8997a9ea67969281543e445a8fe4cf56216c7f5
suniltirwal1012 <suniltirwal1012@gmail.com>
2024-06-16 23:32:38 +0530

    Fixes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': 'https://mern-fullstack-72ou.onrender.com', // Proxy requests with /api to http://localhost:8000
-    },
+    // proxy: {
+    //   '/api': 'https://mern-fullstack-72ou.onrender.com', // Proxy requests with /api to http://localhost:8000
+    // },
   },
   plugins: [react()],
 })
