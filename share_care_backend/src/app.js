73d0fcabf2a4271cfd951c63c6fb52b32c2229a6
suniltirwal1012 <suniltirwal1012@gmail.com>
2024-06-17 12:21:26 +0530

    Cookie

--- share_care_backend/src/app.js
+++ share_care_backend/src/app.js
@@ -7,7 +7,8 @@ const app=express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true,
+    sameSite: 'none',
 }))
 
 app.use(express.json({limit: "16kb"})) 
